c1152e45b8203aaa8c600f28982691819d58b5e7
GDLAB-MSI09\GD Student <zhangyijie16@163.com>
2021-03-24 19:11:29 +0800

    SHVFS_P201_GD06_Q1

---
+++ Assets/SHVFS_P201_GD06_Q1/Quiz1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Quiz1 : MonoBehaviour
+{
+    private int num1;
+    public int Jesson;
+
+    public int AddTogether(int num1, int num2)
+    {
+        return (num1 + num2);
+    }
+
+    private string[] StudentNames = new string[] {"Lyon", "Nan", "Nico", "Jesson", "Jeff"};
+
+    private void Start()
+    {
+        num1 = 5;
+        for (int i = 0; i < StudentNames.Length; i++)
+        {
+            Debug.Log(StudentNames[i]);
+        }
+    }
+}
--- Assets/Script/Components/Wall.cs
+++ Assets/Script/Components/Wall.cs
@@ -13,6 +13,6 @@ public class Wall : BaseGridObject
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
