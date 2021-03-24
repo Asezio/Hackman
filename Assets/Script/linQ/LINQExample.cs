263d8b92f1ad10a3228dfff60a796a8ed378f0e8
GDLAB-MSI09\GD Student <zhangyijie16@163.com>
2021-03-24 19:37:54 +0800

    SHVFS_P201_GD06_Q1

---
+++ Assets/Script/linQ/LINQExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LINQExample : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Data Source
+        var names = new[] {"Bill", "Steve", "James", "Juno"};
+        
+        //Query Syntax
+        var namesWithQuery = from name in names
+            where name.Contains('a')
+            select name;
+        //Method syntax
+        var namesWithAMethod = names.Where(n => n.Contains('a'));
+        foreach (var name in namesWithQuery )
+        {
+            Debug.Log(name);
+        }
+
+        foreach (var name in namesWithAMethod)
+        {
+            Debug.Log(name);
+        }
+    }
+}
